39c82ed2de6cbb15ab35aedbafae30fec1d10791
Richard Kotze <rkotze@dctfh.com>
2017-06-19 22:44:15 +0100

    change default separators to fit querystring as I think this will be the primary default case. Added falsy check.

--- src/join-object.js
+++ src/join-object.js
@@ -1,12 +1,15 @@
-function joinObject(plainObject, separator = DEFAULT_SEPARATOR, keyValSeparator = DEFAULT_SEPARATOR){
+function joinObject(plainObject, propSeparator = DEFAULT_PROP_SEPARATOR, keyValSeparator = DEFAULT_KEY_VALUE_SEPARATOR){
 	let objectAsArray = [];
 	for(let prop in plainObject){
+		if(!plainObject[prop]) continue;
+		
 		objectAsArray.push(prop + keyValSeparator + plainObject[prop]);
 	}
 
-	return objectAsArray.join(separator);
+	return objectAsArray.join(propSeparator);
 }
 
-const DEFAULT_SEPARATOR = ' ';
+const DEFAULT_PROP_SEPARATOR = '&';
+const DEFAULT_KEY_VALUE_SEPARATOR = '=';
 
 export default joinObject;
--- src/join-object.spec.js
+++ src/join-object.spec.js
@@ -1,15 +1,19 @@
 import joinObject from './join-object';
 
 describe('join object', () => {
-	it('should join all key and value of object with a space', () => {
-    joinObject({ chicken: 'burger', spare: 'ribs' }).should.equal('chicken burger spare ribs');
+	it('should join all by default key and value with an equals and props with ampersand', () => {
+    joinObject({ chicken: 'burger', spare: 'ribs' }).should.equal('chicken=burger&spare=ribs');
   });
 
-  it('should join the key and value with a comma and space', () => {
-    joinObject({ chicken: 'burger', spare: 'ribs' }, ',').should.equal('chicken burger,spare ribs');
+  it('should join the key and value with a comma and default props with ampersand', () => {
+    joinObject({ chicken: 'burger', spare: 'ribs' }, ',').should.equal('chicken=burger,spare=ribs');
   });
 
-  it('should join the key and value with a comma and separate with ampersand', () => {
-    joinObject({ chicken: 'burger', spare: 'ribs' }, '&', ',').should.equal('chicken,burger&spare,ribs');
+  it('should join the key and value with a comma and separate with semicolon', () => {
+    joinObject({ chicken: 'burger', spare: 'ribs' }, ';', ',').should.equal('chicken,burger;spare,ribs');
+  });
+
+  it('should not join if value is null', () => {
+    joinObject({ salad: null, chicken: 'burger', spare: 'ribs' }, ';', ',').should.equal('chicken,burger;spare,ribs');
   });
 });
